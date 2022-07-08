426c614fb283a5a4167d159ffa511aaf49209252
German Leonteiv <german_leontiev@mail.ru>
2022-07-09 02:46:22 +0300

    server_setup.sh added

---
+++ server_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt upgrade
+apt install git docker docker-compose
+git clone https://github.com/german-leontiev/reverse-proxy.git
+cd reverse-proxy/
+docker-compose up -d
+docker login
+docker run -dit -p 5003:5003 germanleontiev/bg-rem
+docker run -dit -p 5002:80 germanleontiev/personal_website
