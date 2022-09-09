11b3a99a4ed91268a2b06109d6c4c351d8c0bf76
root <root@folio.local>
2022-09-09 19:58:28 +0000

    gan-spb and minor changes

--- server_setup.sh
+++ server_setup.sh
@@ -7,5 +7,5 @@ cd reverse-proxy/
 docker-compose up -d
 docker login
 docker run -dit -p 5003:5003 germanleontiev/bg-rem
-docker run -dit -p 5020:5020 germanleontiev/assistaint
+docker run -dit -p 8020:8020 --entrypoint bash germanleontiev/assistaint
 docker run -dit -p 5002:80 germanleontiev/personal_website
